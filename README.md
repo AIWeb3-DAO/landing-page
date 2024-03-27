cbfc219d5e91e2b6100f5100087e2088bc5866bb
idfree <151384146+idfree@users.noreply.github.com>
2024-03-26 20:19:02 -0700

    update from CAO
    I have tried to make some updates like the description and also dynamically using different TG link, but somehow I didn't find out a way to find the current website page on TopNavbar, I also tried to make the logo different

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ THIS  WHERE  ALL  ENGLISH TEXT CONTENT  IS  LOCATED
 
 # zh.json
 THIS  WHERE  ALL  CHINESE TEXT CONTENT  IS  LOCATED
+
+
+# Tutorial
+$ npm install
+$ npm run build
+$ lsof -i :3000 and kill -9 id if you need to restart 
+$ npm start 
+
+
+# to do list 
+1. add airdrop/events page that will share the latest updates in the eco and AIWeb3, the main website will share the time, event/airdrop details, rewards, and link, which will redirect to a new website with more details 
+2. the blog only shares top 60 article, at the end of the blog, we can add a button to redirect to the grill space directly so users can check all older articles 
+3. NFT deployment to be done later on 
--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,5 +1,4 @@
 
-
 import Link from 'next/link'
 import React from 'react'
 import {PiTelegramLogo} from 'react-icons/pi'
@@ -8,21 +7,46 @@ import Image from 'next/image'
 import { openLinkInNewTab } from '@/lib/utils'
 import LocaleSwitcher from './LocalSwitcher';
 
+//import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
 
 export default function TopNavbar() {
+  const pathname = usePathname();
+  let locale  = 'zh';
+  if (pathname) {
+    locale = pathname.split('/')[1]; // 'en' or 'zh'
+  }
+  console.log(pathname);
+
+  //type LocaleLinks = {
+  //  en: string;
+  //  zh: string;
+  //};
+  //const searchParams = useSearchParams();
+  //const locale = searchParams.get('locale') || 'zh'; // Default to 'zh' if not found
+  // Define your Telegram links based on locale
+  //const locale = 'zh'
+  const TGLink = {
+    en: 'https://t.me/aiweb3dao_eng',
+    zh: 'https://t.me/AIweb3dao', // Example link for Chinese locale
+  };
+  // Use the current locale to get the correct Twitter link
+  const currentTGLink = TGLink[locale as keyof typeof TGLink] || TGLink.zh; // Default to Chinese if undefined
   return (
     <div className='w-full bg-gray-800/80 sticky top-0  z-20'>
     <div className='  border-b border-b-gray-800/30 flex justify-between items-center py-5 px-3 h-[65px]  max-w-6xl w-full  mx-auto'>
        <Link href={`/`}>
        <div className='flex items-center gap-2'>
          <Image
-          width={30}
-          height={30}
+          width={90}
+          height={90}
           alt='logo'
-          src={`https://pbs.twimg.com/profile_images/1637269369658171392/CHS2SlUh_400x400.jpg`}
+          src={`/img/logo3.png`}
           className='rounded-full '
          />
-       <h1 className='font-semibold'>Ai web3</h1>
+       <h1 className='font-semibold'>AIWeb3 DAO</h1>
        </div>
        </Link>
       
@@ -32,14 +56,25 @@ export default function TopNavbar() {
 
            </div>
           <div className=' items-center gap-3 hidden md:flex'>
-            <div className='cursor-pointer  w-[30px] h-[30px] flex items-center justify-center rounded-full ring-rose-400 hover:ring-1' >
-            <RiTwitterXLine style={{width : "20px", height : "20px "}} />
-            </div>
-            
-             <div className='cursor-pointer  w-[30px] h-[30px] flex items-center justify-center rounded-full ring-rose-400 hover:ring-1' >
-             <PiTelegramLogo style={{width : "20px", height : "20px"}} />
-             </div>
-            
+            <a href="https://twitter.com/aiweb3dao" target="_blank" rel="noopener noreferrer">
+              <div className='cursor-pointer  w-[30px] h-[30px] flex items-center justify-center rounded-full ring-rose-400 hover:ring-1' >
+              <RiTwitterXLine style={{width : "20px", height : "20px "}} />
+              </div>
+            </a>
+
+
+            <a href={currentTGLink} target="_blank" rel="noopener noreferrer">
+                <div className='cursor-pointer  w-[30px] h-[30px] flex items-center justify-center rounded-full ring-rose-400 hover:ring-1' >
+                <PiTelegramLogo style={{width : "20px", height : "20px"}} />
+                </div>
+            </a>
+
+            <a href="https://discord.gg/pQtZG8UQfk" target="_blank" rel="noopener noreferrer">
+                <div className='cursor-pointer  w-[30px] h-[30px] flex items-center justify-center rounded-full ring-rose-400 hover:ring-1' >
+                <RiDiscordLine style={{width : "20px", height : "20px"}} />
+                </div>
+            </a>
+
              <LocaleSwitcher  />
           </div>
     </div>
